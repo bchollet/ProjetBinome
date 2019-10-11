3b3abae6f2a80b303db256260b45891f4c1a22d4
bastian.chollet <babacouda@gmail.com>
2019-10-11 15:00:44 +0200

    Authentif. des users cliquant sur le lien dans leur boîte mail

--- verifMail.php
+++ verifMail.php
@@ -5,5 +5,17 @@
  * Date: 02.10.2019
  * Time: 10:19
  */
-echo $_GET['code'];
+
+include 'ConnectDB.php';
+
+$activationCode = $_GET['code'];
+
+$blogitoDB->query("UPDATE users SET user_verified = 1 WHERE verification_code = '" . $activationCode . "';");
+
+echo 'Votre compte a été validé ! Vous pouvez désormais vous connecter en utilisant vos identifiants';
+
 ?>
+
+<a href="index.php">
+    <button>Retour menu</button>
+</a>
