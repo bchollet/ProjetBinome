9c062adc3f822d7239964f6b345edcd6ae997e4c
Bastian <bastian.chollet@cpnv.ch>
2019-11-03 15:23:41 +0100

    Création d'un répertoire à la vérif. de l'utilisateur

--- verifMail.php
+++ verifMail.php
@@ -17,10 +17,15 @@ if (!empty($row)) {
     $blogitoDB->query("INSERT INTO pages VALUES (null, 'default')");
     $lastInsertId = $blogitoDB->lastInsertId();
     $blogitoDB->query("UPDATE users SET user_verified = 1, pages_id = " . $lastInsertId . " WHERE verification_code = '" . $activationCode . "';");
+
+    //Création d'un répertoire sur le serveur contenant les images
+    mkdir("server/img/" . $row['username']);
+    mkdir("server/img/" . $row['username'] . "/large");
+    mkdir("server/img/" . $row['username'] . "/thumb");
 }
 
 else {
-    header("Location:login.php?qErrVerif=true");
+    header("Location:login.php?qErrRegister=10");
     exit();
 }
 
