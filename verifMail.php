031034af0cad558d24f9ee25abf7df2968b5f424
Bastian <bastian.chollet@cpnv.ch>
2019-11-03 15:10:37 +0100

    Attribution d'une page aux utilisateurs vérifiés

--- verifMail.php
+++ verifMail.php
@@ -10,7 +10,19 @@ include 'ConnectDB.php';
 
 $activationCode = $_GET['code'];
 
-$blogitoDB->query("UPDATE users SET user_verified = 1 WHERE verification_code = '" . $activationCode . "';");
+$result = $blogitoDB->query("SELECT username FROM users WHERE verification_code = '" . $activationCode . "';");
+$row = $result->fetch();
+
+if (!empty($row)) {
+    $blogitoDB->query("INSERT INTO pages VALUES (null, 'default')");
+    $lastInsertId = $blogitoDB->lastInsertId();
+    $blogitoDB->query("UPDATE users SET user_verified = 1, pages_id = " . $lastInsertId . " WHERE verification_code = '" . $activationCode . "';");
+}
+
+else {
+    header("Location:login.php?qErrVerif=true");
+    exit();
+}
 
 echo 'Votre compte a été validé ! Vous pouvez désormais vous connecter en utilisant vos identifiants';
 
