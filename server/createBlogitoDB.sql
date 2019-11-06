48b68de3c73e6c2540d1c9274e66f754a559e81f
bastian.chollet <babacouda@gmail.com>
2019-11-06 11:51:17 +0100

    Nettoyage dans le répertoire du projet

--- server/createBlogitoDB.sql
+++ server/createBlogitoDB.sql
@@ -1,37 +1,46 @@
-CREATE TABLE IF NOT EXISTS users(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	username VARCHAR(45) NOT NULL,
-	password VARCHAR(45) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	admin TINYINT(1) NOT NULL,
-	pages_id INTEGER,
-	FOREIGN KEY (pages_id) REFERENCES pages(id)
-);
-CREATE TABLE IF NOT EXISTS pages(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	color VARCHAR(45) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS publications(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	pictureSrc VARCHAR(150),
-	pages_id INTEGER,
-	FOREIGN KEY(pages_id) REFERENCES pages(id)
-);
-CREATE TABLE IF NOT EXISTS comments(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	comment LONGTEXT NOT NULL,
-	users_id INTEGER NOT NULL,
-	publications_id INTEGER NOT NULL,
-	FOREIGN KEY(users_id) REFERENCES users(id),
-	FOREIGN KEY(publications_id) REFERENCES publications(id)
+create table pages
+(
+  id    INTEGER     not null
+    primary key
+  autoincrement,
+  color VARCHAR(45) not null
 );
 
+create table publications
+(
+  id         INTEGER not null
+    primary key
+  autoincrement,
+  pictureSrc VARCHAR(300),
+  thumbSrc   VARCHAR(300),
+  pages_id   INTEGER
+    references pages
 
--- Ajout des Administrateurs au moment de la création de la DB
-INSERT INTO users (id, username, password, email, admin)
-VALUES (null, 'Milos', 'Pa$$w0rd', 'milos.cerovic@cpnv.ch', 1); 
+);
 
-INSERT INTO users (id, username, password, email, admin)
-VALUES (null, 'Bastian', 'Pa$$w0rd', 'bastian.chollet@cpnv.ch', 1);
+create table users
+(
+  id                INTEGER      not null
+    primary key
+                               autoincrement,
+  username          VARCHAR(45)  not null,
+  password          VARCHAR(255) not null,
+  email             VARCHAR(100) not null,
+  admin             TINYINT(1)   not null,
+  pages_id          INTEGER
+    references pages,
+  verification_code VARCHAR(32),
+  user_verified     TINYINT(1) default 0
+);
 
-
+create table comments
+(
+  id              INTEGER  not null
+    primary key
+  autoincrement,
+  comment         LONGTEXT not null,
+  users_id        INTEGER  not null
+    references users,
+  publications_id INTEGER  not null
+    references publications
+);
